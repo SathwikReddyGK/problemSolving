99316d92adf4e1a89ad4cc4747fe0593024800c2
SathwikReddyGK <sathwikreddy.gk@gmail.com>
2023-10-19 13:16:50 -0500

    leetcode problem 66

--- plusOneLeet66.py
+++ plusOneLeet66.py
@@ -1,19 +1,25 @@
+import math
 def plusOne(digits):
-        modvalue = 0
-        for i in range(-1,-(len(digits)+1)):
-            intermediate = digits[i] + 1 + modValue
+        modValue = 0
+        for i in range(-1,-(len(digits)+1),-1):
+            if i == -1:
+                intermediate = digits[i] + 1 + modValue
+            else:
+                intermediate = digits[i] + modValue 
             if intermediate > 9:
                 modValue = intermediate%10
                 digits[i] = modValue
-                modValue = floor(intermediate/10)
+                modValue = math.floor(intermediate/10)
             else:
+                digits[i] = intermediate
                 modValue = 0
-            if i == -(len(digits)+1) and modValue != 0:
-                digits.insert(0,modValue)
+                break
+        if modValue != 0:
+            digits.insert(0,modValue)
 
         
         return digits
 
 if __name__ == "__main__":
-     digits = [1,2,3]
+     digits = [9,9,9]
      print(plusOne(digits))
