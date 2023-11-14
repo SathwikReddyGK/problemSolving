07b7b0eec646194850d87fc0ddd9b19ac48a74ac
SathwikReddyGK <sathwikreddy.gk@gmail.com>
2023-11-14 16:58:39 -0600

    leet53

--- Medium Problems/leet53_Maximum_Subarray.py
+++ Medium Problems/leet53_Maximum_Subarray.py
@@ -1,16 +1,30 @@
 def maxSubArray(nums):
-    tempSum = 0
-    for num in nums:
-        if tempSum < 0 and num < 0 and num > tempSum:
-            tempSum = num
-        elif tempSum < 0 and num >= 0:
-            tempSum = num
-        else:
-            tempSum += num
     
-    return tempSum
+    # As I wrote below solution, since too many edge cases handled separetely wanted to see if there is a better solution, implemented with help fo Sudharshan
+    tempSum = nums[0]
+    sum = nums[0]
+    for i in range(1,len(nums)):  
+        tempSum = max(0, tempSum)       
+        tempSum += nums[i]     
+        
+        sum = max(tempSum,sum)    
+    return sum
+    # My Solution
+    # tempSum = nums[0]
+    # sum = nums[0]
+    # for i in range(1,len(nums)):#num in nums:
+    #     if tempSum < 0 and nums[i] < 0 and nums[i] > tempSum:
+    #         tempSum = nums[i]
+    #     elif tempSum < 0 and nums[i] >= 0:
+    #         tempSum = nums[i]
+    #     else:
+    #         tempSum += nums[i]
+        
+    #     sum = max(tempSum,sum)
+    # sum = max(tempSum,sum)
+    # return sum
 
 
 if __name__ == "__main__":
-    nums = [-2,1,-3,4,-1,2,1,-5,4]
+    nums = [-1,-2,-3]
     print(maxSubArray(nums))
--- Medium Problems/tempCodeRunnerFile.py
+++ Medium Problems/tempCodeRunnerFile.py
@@ -1,36 +1,8 @@
-# Same solution as before, but from user solutions got a hint that I can sort the input itself
-    # so trying if sorting input would help save time
-    numDict = {}
-    dupTripList = []
-    tempTripletDict = {}
-    res = []
-
-    nums.sort()
-    print(nums)
-    for i in range(0,len(nums)):
-        numDict[nums[i]] = i
-
-    listLength = len(nums)
-
-    for i in range(0,listLength):
-        if i>0 and nums[i] == nums[i-1]:
-            continue
-        for j in range(i+1,listLength):
-            sum2 = nums[i] + nums[j]
-
-            if ( sum2 != 0 and -sum2 in numDict and numDict[-sum2] > i and numDict[-sum2] != j ) or (sum2 == 0 and sum2 in numDict and numDict[sum2] > i and numDict[sum2] != j):
-                concat = str(nums[i])+str(nums[j])+str(-sum2)
-                if concat not in tempTripletDict:
-                    tempTripletDict[concat] = 1
-                    dupTripList.append([nums[i],nums[j],-sum2])
-
-    print(tempTripletDict)
-
-    # for i in range(0,len(dupTripList)):
-    #     dupTripList[i].sort()
-    #     concat = str(dupTripList[i][0])+str(dupTripList[i][1])+str(dupTripList[i][2])
-    #     if concat not in tempTripletDict:
-    #         tempTripletDict[concat] = 1
-    #         res.append(dupTripList[i])
-    
-    return dupTripList
+tempSum = nums[0]
+    sum = nums[0]
+    for i in range(1,len(nums)):  
+        tempSum = max(0, tempSum)       
+        tempSum += nums[i]     
+        
+        sum = max(tempSum,sum)    
+    return sum
