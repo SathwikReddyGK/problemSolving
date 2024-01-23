f3cd2acadf9d595cc5106ee7b35086e591b80519
SathwikReddyGK <sathwikreddy.gk@gmail.com>
2024-01-22 22:05:55 -0600

    S30 Competative Coding 5 assignment, leet36 and 515 completed

--- Medium Problems/leet36_Valid_Sudoku.py
+++ Medium Problems/leet36_Valid_Sudoku.py
@@ -45,39 +45,97 @@
 # board[i].length == 9
 # board[i][j] is a digit 1-9 or '.'.
 
+# Solution
+
+# For S30 Solution
+# // Time Complexity : O(M*N) perfect is, it is 3*M*N since we are iterating over the matrix thrice to validate rows, columns
+#                      and boxes
+# // Space Complexity : O(1), since we are creating constant boolean list of size 9
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : None
+
+
+# // Your code here along with comments explaining your approach
+# Approach is to iterate over rows and columns while having a boolean list for each row and column when we are processing that
+# perticular row and column. Update boolean list to see if a number is already used, if it is already set to True then
+# return false, else keep going. For box do that same activity, only thing is inner loop ranges should be calculated using 
+# the block number we are currently processing. rowStart = (block/3)*3, rowEnd = ((block/3)*3) + 3
+# colStart = (block%3)*3, colStart = ((block%3)*3)+3
+
 def isValidSudoku(board):
+
+    # S30
+    rowNums = len(board)
+    colNums = len(board[0])
+
+    if rowNums != 9 or colNums != 9:
+        return False
+
+    for i in range(rowNums):
+        boolean = [False for _ in range(9)]
+        for j in range(colNums):
+            if board[i][j] == ".":
+                continue
+            if boolean[int(board[i][j])-1] == True:
+                return False
+            
+            boolean[int(board[i][j])-1] = True
+    
+    for i in range(colNums):
+        boolean = [False for _ in range(9)]
+        for j in range(rowNums):
+            if board[j][i] == ".":
+                continue
+            if boolean[int(board[j][i])-1] == True:
+                return False
+            
+            boolean[int(board[j][i])-1] = True
+
+    for block in range(9):
+        boolean = [False for _ in range(9)]
+        for i in range((block//3)*3,((block//3)*3)+3):
+            for j in range((block%3)*3,((block%3)*3)+3):
+                if board[i][j] == ".":
+                    continue
+                
+                if boolean[int(board[i][j])-1] == True:
+                    return False
+
+                boolean[int(board[i][j])-1] = True
+
+    return True
 
 # Implementing the same with small changes to see if it improves performance
 # (checking c and using continue instead of checking it using if condition each time saved around 20ms)
-    rows = []
-    cols = [{} for _ in range(0,9)]
-    box = [{} for _ in range(0,9)]
+    # rows = []
+    # cols = [{} for _ in range(0,9)]
+    # box = [{} for _ in range(0,9)]
 
-    for row in range(0,9):
-        rowDict = {}
-        for col in range(0,9):
-            c = board[row][col]
-            if c == ".":
-                continue
-            if c in rowDict:
-                return False
-            else:
-                rowDict[c] = 1
+    # for row in range(0,9):
+    #     rowDict = {}
+    #     for col in range(0,9):
+    #         c = board[row][col]
+    #         if c == ".":
+    #             continue
+    #         if c in rowDict:
+    #             return False
+    #         else:
+    #             rowDict[c] = 1
             
-            if c in cols[col]:
-                return False
-            else:
-                cols[col][c] = 1
+    #         if c in cols[col]:
+    #             return False
+    #         else:
+    #             cols[col][c] = 1
             
-            boxNum = 3*(row//3) + (col//3)
+    #         boxNum = 3*(row//3) + (col//3)
 
-            if c in box[boxNum]:
-                return False
-            else:
-                box[boxNum][c] = 1
+    #         if c in box[boxNum]:
+    #             return False
+    #         else:
+    #             box[boxNum][c] = 1
             
-        rows.append(rowDict)
-    return True
+    #     rows.append(rowDict)
+    # return True
 
     # Solution implemented by discussing with Sudharshan
     # He suggested to use the 2D approach which saved lot of time and provided approach
