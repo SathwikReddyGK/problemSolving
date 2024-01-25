ad29e4a9c94262b918304a6fc7d3845bb2344072
SathwikReddyGK <sathwikreddy.gk@gmail.com>
2024-01-25 01:22:06 -0600

    S30 Assignment DP-1, leet198 House robber,leet322 Coin Change completed

--- Medium Problems/tempCodeRunnerFile.py
+++ Medium Problems/tempCodeRunnerFile.py
@@ -1,3 +1,9 @@
-
-        print(head.val)
-        head = head.next
+def helper(nums,pivot,path,result):
+        for i in range(pivot,len(nums)):
+            path.append(nums[i])
+            result.append(path.copy())
+            helper(nums,i+1,path,result)
+            path.pop()
+    result = [[]]
+    helper(nums,0,[],result)
+    return result
